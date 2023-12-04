cc3d7b231f6f87b8c0278d6cc0f796ea828580e8
Colm Murphy <colmmurphy016@gmail.com>
2023-12-04 14:29:14 +0000

    day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -2,20 +2,33 @@ fun main() {
     fun part1(input: List<String>): Int {
         var sum = 0
         for (line in input) {
-            val filtered= line.filter { it -> it.isDigit() }
-            sum += filtered.toInt()
+            val filtered = line.filter { it.isDigit() }
+            sum += (filtered.first().digitToInt() * 10) + filtered.last().digitToInt()
         }
+        return sum
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val sanitisedInput = input.toMutableList()
+        for (i in sanitisedInput.indices) {
+            val line = sanitisedInput[i]
+            sanitisedInput[i] = line.replace(Regex("(?:one|two|three|four|five|six|seven|eight|nine)")) { it: MatchResult ->
+                (listOf("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+                    .indexOf(it.value) + 1).toString()
+            }
+//            println(sanitisedInput[i])
+        }
+        return part1(sanitisedInput)
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
     val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+
+//    println("Part 1 Test: ${part1(testInput)}")
+//    println("Part 1: ${part1(input)}")
+
+    println("Part 2 Test: ${part2(testInput)}")
+    println("Part 2: ${part2(input)}")
+
 }
