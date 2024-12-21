fc182955b92c649844f6b19657c97f8766f58074
Colm Murphy <colmmurphy016@gmail.com>
2024-12-21 14:43:48 +0000

    add usage

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,6 @@
 module Main where
 import System.Environment (getArgs)
+import System.Exit (exitWith, ExitCode( ExitFailure ))
 import Text.Read (readMaybe)
 
 import Day01 (day01)
@@ -14,12 +15,18 @@ import Day09 (day09)
 import Day11 (day11)
 import Day14 (day14)
 
+usage :: IO ()
+usage = do
+    print "Usage: aoc2024 <DAY_NUMBER>"
+    print "Day must be an integer between 1 and 25."
+    exitWith $ ExitFailure 1
+
 main :: IO ()
 main = do
     args <- getArgs
     let dayNumber = readMaybe (head args) :: Maybe Int
     case dayNumber of
-        Nothing -> error "Bad Input"
+        Nothing -> usage
         Just n -> case n of
             1 -> day01
             2 -> day02
@@ -30,6 +37,20 @@ main = do
             7 -> day07
             8 -> day08
             9 -> day09
+            10 -> error "not implemented"
             11 -> day11
+            12 -> error "not implemented"
+            13 -> error "not implemented"
             14 -> day14
-            _ -> error "Invalid input arguments"
+            15 -> error "not implemented"
+            16 -> error "not implemented"
+            17 -> error "not implemented"
+            18 -> error "not implemented"
+            19 -> error "not implemented"
+            20 -> error "not implemented"
+            21 -> error "not implemented"
+            22 -> error "not implemented"
+            23 -> error "not implemented"
+            24 -> error "not implemented"
+            25 -> error "not implemented"
+            _ -> usage
