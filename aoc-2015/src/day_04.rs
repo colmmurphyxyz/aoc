a76600d1cf94ea200aba519e11dc679e719147b7
Colm Murphy <colmmurphy016@gmail.com>
2025-12-23 23:49:41 +0000

    aoc 2015: parallelise day 4

--- aoc-2015/src/day_04.rs
+++ aoc-2015/src/day_04.rs
@@ -1,25 +1,32 @@
 use md5::compute;
+use rayon::prelude::*;
 
 static INPUT: &str = "yzbqklnj";
 
 pub fn day04_a() -> i64 {
-    let mut x = 0;
-    loop {
-        let hash = compute(format!("{}{}", INPUT, x)).0;
-        if hash[0] == 0 && hash[1] == 0 && hash[2] < 16 {
-            return x;
-        }
-        x += 1;
+    fn predicate(hash: [u8; 16]) -> bool {
+        hash[0] == 0 && hash[1] == 0 && hash[2] < 16
     }
+    let result = (1i64..)
+        .par_bridge()
+        .find_any(|&n| {
+            let digest = compute(format!("{}{}", INPUT, n));
+            predicate(digest.0)
+        });
+
+    result.unwrap()
 }
 
 pub fn day04_b() -> i64 {
-    let mut x = 0;
-    loop {
-        let hash = compute(format!("{}{}", INPUT, x)).0;
-        if hash[0] == 0 && hash[1] == 0 && hash[2] == 0 {
-            return x;
-        }
-        x += 1;
+    fn predicate(hash: [u8; 16]) -> bool {
+        hash[0] == 0 && hash[1] == 0 && hash[2] == 0
     }
+    let result = (1i64..)
+        .par_bridge()
+        .find_any(|&n| {
+            let digest = compute(format!("{}{}", INPUT, n));
+            predicate(digest.0)
+        });
+
+    result.unwrap()
 }
